26639b5ba7ee7a2ae19652219fb32626343e04c1
Aman Thukral <thukral618@gmail.com>
2024-02-11 02:46:13 +0530

    Adding method for login using email and password and using JWT

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,8 @@ import userRoutes from "./routes/userRoutes.js";
 const port = process.env.PORT || 8000;
 connectDB();
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
