d3a85928e2c0130ec8dad12067ca3710ea681bc6
Aman Thukral <thukral618@gmail.com>
2024-02-26 02:19:19 +0530

    Adding Admin route

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="px-[3rem] bg-slate-300 w-full fixed bottom-0 mt-2">
+    <footer className="px-[3rem] bg-slate-300 w-full absolute bottom-0">
       <div className="text-center">TeeVolution &copy; {currentYear}</div>
     </footer>
   );
