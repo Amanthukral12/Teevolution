3f24df74dad773ec33d4c8f6350f9b661b989ad5
Aman Thukral <thukral618@gmail.com>
2024-03-02 15:21:44 +0530

    Adding backend for creating product review

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,6 +8,7 @@ import {
   getProductById,
   getProducts,
   updateProduct,
+  createProductReview,
 } from "../controllers/productControllers.js";
 
 import { protect, admin } from "../middleware/authMiddleware.js";
@@ -19,5 +20,7 @@ router
   .get(getProductById)
   .put(protect, admin, updateProduct)
   .delete(protect, admin, deleteProduct);
+
+router.route("/:id/reviews").post(protect, createProductReview);
 
 export default router;
