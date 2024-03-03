3382b04eda7f01667ee1de6bf91d005fc0f7487f
Aman Thukral <thukral618@gmail.com>
2024-03-03 13:55:26 +0530

    Updating the logic for pagination

--- frontend/src/pages/ProductList.jsx
+++ frontend/src/pages/ProductList.jsx
@@ -1,10 +1,12 @@
+import { useParams } from "react-router-dom";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import Product from "../components/Product";
 import { useGetProductsQuery } from "../slices/productApiSlice";
 
 const ProductList = () => {
-  const { data: products, isLoading, error } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+  const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
 
   return (
     <section className="px-[3rem] pb-[2rem]">
@@ -18,7 +20,7 @@ const ProductList = () => {
         <>
           <h1 className="text-center py-20">New Arrivals</h1>
           <div className="flex flex-wrap justify-evenly">
-            {products.map((product) => (
+            {data.products.map((product) => (
               <Product product={product} key={product._id} />
             ))}
           </div>
