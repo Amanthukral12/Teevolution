29890976b0b3cd90ee3b78d785176cdbd8703a06
Aman Thukral <thukral618@gmail.com>
2024-03-07 19:54:50 +0530

    Adding middleware to check valid object id is present or not

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -8,12 +8,6 @@ const errorHandler = (err, req, res, next) => {
   let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   let message = err.message;
 
-  //check for mongoose bad object ID
-  if (err.name === "CastError" && err.kind === "ObjectId") {
-    message = `Resource not Found`;
-    statusCode = 404;
-  }
-
   res.status(statusCode).json({
     message,
     stack: process.env.NODE_ENV === "production" ? "🎂" : err.stack,
